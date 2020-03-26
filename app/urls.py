417a01430b20170662e46cb87ea17369215f2c43
nguyen.trong.toan <toannt181@gmail.com>
2020-03-26 17:16:25 +0700

    tempo add restful

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from .views.company import CompanyView
+from .views.company import company_collection, company_element
 
 urlpatterns = [
-    path('companies/', CompanyView.as_view(), name='company-index'),
+    path('companies/', company_collection, name='company-index'),
+    path('companies/<int:company_id>', company_element, name='company-index'),
 ]
--- app/views/company.py
+++ app/views/company.py
@@ -1,8 +1,28 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.views import View
 from ..models import Company
 
-class CompanyView(View):
+def index(request):
+    return JsonResponse(list(Company.objects.values()), safe=False)
 
-    def get(self, request):
-        return JsonResponse(list(Company.objects.values()), safe=False)
+def store(request):
+    return JsonResponse(list(Company.objects.values()), safe=False)
+
+def company_collection(request):
+    if request.method == 'GET':
+        return index(request)
+    elif request.method == 'POST':
+        return index(request)
+
+    raise Http404()
+
+def company_element(request):
+    if request.method == 'GET':
+        return index(request)
+    # elif request.method == 'post':
+        # return index(request)
+
+    return Http404()
+
+def show(request, company_id):
+    return JsonResponse(Company.objects.values().get(pk=company_id), safe=False)
